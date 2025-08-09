034573ad3b89674ad341118fc23aca3a8a3b8faa
akhilanil <akhilanil95@gmail.com>
2025-08-10 02:30:14 +0530

    Docs

--- backend/src/main/java/com/liftlab/controller/DashboardController.java
+++ backend/src/main/java/com/liftlab/controller/DashboardController.java
@@ -5,8 +5,6 @@ import com.liftlab.models.UserDetailsResponse;
 import com.liftlab.models.PageViewsResponse;
 import com.liftlab.service.DashboardService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
